5e9bac333bc9e99edd96e8a0b5fd005be34ce84f
John Biggerstaff <john.r.biggerstaff@gmail.com>
2022-09-03 10:23:24 -0600

    inital commit

---
+++ classes-demo.js
@@ -0,0 +1,11 @@
+const person = {
+    'firstName': 'Chandler';
+    "lastName": 'Bing';
+    age: 30 //prefer
+}
+
+const personAge: 'age'
+
+console.log(person.firstName)
+console.log(person['lastName'])
+console.log(person[personAge])
